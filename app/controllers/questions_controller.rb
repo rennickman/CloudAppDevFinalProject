d806d39666983ee3118e01692c7de40c72899600
Rennlad <rennickman@gmail.com>
2022-05-06 23:40:40 +0100

    added friendly_urls

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -77,7 +77,7 @@ class QuestionsController < ApplicationController
 
         # Use callbacks to share common setup or constraints between actions.
         def set_question
-            @question = Question.find(params[:id])
+            @question = Question.friendly.find(params[:id])
         end
 
         # Only allow a list of trusted parameters through.
